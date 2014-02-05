f570614c3521ba799e5e4b5ea913b548b207d870
Mohammed Habib <moe.habib9@gmail.com>
2014-02-05 22:25:27 +0000

    added url parsing for authentication check

--- client-only/src/js/app.js
+++ client-only/src/js/app.js
@@ -4,12 +4,11 @@ var InstagramApp = (function(window, document, $) {
 		// The application client id
 		client_id: "1780a9ec5baf4cd8b0b934fccd0b3580",
 
-		// Id of the authentication button
-		authenticate_btn_id: "instagram_get_btn",
-
+		redirect_uri: "http://sane.mooo.com/instagram-test/client-only/src/",
 		instagram_auth_url: "https://instagram.com/oauth/authorize/?client_id={{CLIENT-ID}}&redirect_uri={{REDIRECT-URI}}&response_type=token",
 
-		redirect_uri: "http://sane.mooo.com/instagram-test/client-only/src/",
+		// Id of the authentication button
+		authenticate_btn_id: "instagram_get_btn",
 
 		init: function() {
 			var ia = InstagramApp,
@@ -18,22 +17,24 @@ var InstagramApp = (function(window, document, $) {
 
 			btn = $("#" + ia.authenticate_btn_id);
 
-			btn.on("click", ia.authenticate);
+			btn.on("click", ia.get_images);
 		},
 
 		// Authenticate the current instagram user (if not authenticated already)
-		authenticate: function() {
+		get_images: function() {
 			var ia = InstagramApp,
-				url = ia.craft_authentication_url(),
+				auth_url = ia.craft_authentication_url(),
 				token;
 
 			token = ia.get_access_token()
 
 			if (token == null)	{	
-				window.location = url;
+				// need to authenticate first!
+				window.location = auth_url;
 			}
 			else {
-				return token;
+				// Fetch the images here
+				
 			}
 
 		},
@@ -50,9 +51,27 @@ var InstagramApp = (function(window, document, $) {
 		},
 
 		get_access_token: function() {
-			var a = document.createElement("a");
+			var a = document.createElement("a"),
+					token;
+
+			// Parsing the current url to check if the access token already exists
 			a.href = window.location;
 			console.log(a.hash);
+			// Removing the hash
+			token = a.hash.substr();
+			token = token.split('=');
+
+			if (token.length == 2) {
+				if token[0] == 'access_token' {
+					return token[1];
+				}
+				else {
+					return null;
+				}
+			}
+			else {
+				return null
+			}
 		}
 
 	}
